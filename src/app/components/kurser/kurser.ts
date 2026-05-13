ab23865d61eb691b1b51a360fe0d5c135fa659d4
runegranath <rune.granath@gmail.com>
2026-05-13 13:20:56 +0200

    kurssidan laddar kurser med kort-layout som css

--- src/app/components/kurser/kurser.ts
+++ src/app/components/kurser/kurser.ts
@@ -1,10 +1,34 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { CourseService } from '../../services/course';
+import { Course } from '../../models/course';
 
 @Component({
   selector: 'app-kurser',
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './kurser.html',
   styleUrl: './kurser.scss',
 })
-export class Kurser {}
+export class Kurser {
+  // Variabel för datan
+  courseList: Course[] = [];
+
+  // injicera service
+  private courseService = inject(CourseService);
+
+  ngOnInit(): void {
+    this.getCourseData();
+  }
+
+  getCourseData(): void {
+    this.courseService.getCourses().subscribe({
+      next: (data) => {
+        this.courseList = data; // länka api-data till variabel
+      },
+      error: (err) => {
+        console.error('Något gick fel:', err);
+      },
+    });
+  }
+}

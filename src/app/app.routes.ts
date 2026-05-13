3883209b9b98366044b53a16a0d422ce700f6720
runegranath <rune.granath@gmail.com>
2026-05-13 10:35:17 +0200

    nav klar med css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { Kurser } from './components/kurser/kurser';
 import { Ramschema } from './components/ramschema/ramschema';
+import { NotFound } from './components/not-found/not-found';
 
 export const routes: Routes = [
     { path: 'kurser', component: Kurser },
     { path: 'ramschema', component: Ramschema },
     { path: '', redirectTo: '/kurser', pathMatch: 'full' },
-    { path: '**', redirectTo: '/kurser'}
+    { path: '**', component: NotFound }
 ];
--- src/app/components/kurser/kurser.ts
+++ src/app/components/kurser/kurser.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-kurser',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './kurser.html',
   styleUrl: './kurser.scss',
 })
